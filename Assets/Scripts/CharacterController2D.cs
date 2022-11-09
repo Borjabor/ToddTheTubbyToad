0da48f8b1504c91c887163bc6e0fa42b3f3c52a9
André Borja Miranda <andre.borja.miranda@gmail.com>
2022-11-09 12:32:56 -0800

    Included Checkpoint, Respawn, and Death triggers

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -23,7 +23,7 @@ public class CharacterController2D : MonoBehaviour
 	const float _groundedRadius = .2f; // Radius of the overlap circle to determine if grounded
 	private bool _grounded;            // Whether or not the player is grounded.
     private Vector2 _checkpoint;
-    private bool _isRespawning = false;
+    public static bool _isRespawning = false;
     [SerializeField]
     private float _iFramesDuration;
     [SerializeField]
@@ -124,12 +124,12 @@ public class CharacterController2D : MonoBehaviour
         _horizontalMove = Input.GetAxis("Horizontal") * _moveSpeed;
     }
 
-	/*private void OnTriggerEnter2D(Collider2D other)
+	private void OnTriggerEnter2D(Collider2D other)
 	{
 
 		if (other.gameObject.CompareTag("Checkpoint"))
 		{
-			_audioSource.PlayOneShot(_checkpointAudio);
+			//_audioSource.PlayOneShot(_checkpointAudio);
 			_checkpoint = other.transform.position;
 		}
 		
@@ -141,66 +141,41 @@ public class CharacterController2D : MonoBehaviour
 		}
 	}
 
-	private void OnCollisionStay2D(Collision2D other)
-	{
-		foreach(ContactPoint2D hitPos in other.contacts)
-		{
-            if(hitPos.normal.y <= 0  && other.gameObject.CompareTag("Enemy"))
-            {
-                StartCoroutine(Respawn());
-            }
-			
-			else if(hitPos.normal.y > 0  && other.gameObject.CompareTag("Enemy"))
-            {
-                _rb.velocity = Vector2.up * (_jumpForce/2);
-            }
-        }
-	}
-
 	private void OnCollisionEnter2D(Collision2D other)
 	{
-		if(other.gameObject.CompareTag("Spikes") && !_isRespawning)
+		if(other.gameObject.CompareTag("Hazard") && !_isRespawning)
 		{
-            //_rb.AddForce(new Vector2(-transform.localScale.x * _knockbackX, _knockbackY), ForceMode2D.Impulse);
             StartCoroutine(Respawn());
         }
 
-		foreach(ContactPoint2D hitPos in other.contacts)
-		{
-            if(hitPos.normal.y <= 0  && other.gameObject.CompareTag("Enemy"))
-            {
-	            Physics2D.IgnoreLayerCollision(0, 6, true);
-                StartCoroutine(Respawn());
-            }
-			else if(hitPos.normal.y > 0  && other.gameObject.CompareTag("Enemy"))
-            {
-                _rb.velocity = Vector2.up * (_jumpForce/2);
-            }
-        }
+		// foreach(ContactPoint2D hitPos in other.contacts)
+		// {
+  //           if(hitPos.normal.y <= 0  && other.gameObject.CompareTag("Enemy"))
+  //           {
+	 //            Physics2D.IgnoreLayerCollision(0, 6, true);
+  //               StartCoroutine(Respawn());
+  //           }
+		// 	else if(hitPos.normal.y > 0  && other.gameObject.CompareTag("Enemy"))
+  //           {
+  //               _rb.velocity = Vector2.up * (_jumpForce/2);
+  //           }
+  //       }
 		
-	}*/
+	}
 
 	private IEnumerator Respawn()
 	{
 		_isRespawning = true;
-		_liveCount.LoseLife();
 		_rb.velocity = Vector2.zero;
 		_audioSource.PlayOneShot(_deathAudio);
 		_characterSprite.SetActive(false);
 		_deathParticles.Play();
 		yield return new WaitForSeconds(1.5f);
-		if(_liveCount._remainingLives <= 0)
-		{
-			_liveCount._remainingLives = 5;
-			transform.position = _checkpoint;
-		}
+		transform.position = _checkpoint;
 		_characterSprite.SetActive(true);
 		_isRespawning = false;
-		
-        
 		StartCoroutine(Invulnerability());
-
-    }
+	}
 
 	private IEnumerator Invulnerability()
 	{

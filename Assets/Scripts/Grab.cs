3db59d0467447d87d53e9c47a6647fa27f77cb6f
AndreBorja <andre.borja.miranda@gmail.com>
2022-11-17 12:19:26 -0800

    Improved Grab

--- Assets/Scripts/Grab.cs
+++ Assets/Scripts/Grab.cs
@@ -7,6 +7,8 @@ public class Grab : MonoBehaviour
 {
     [HideInInspector]
     public static bool _isHolding = false;
+
+    [SerializeField] private CircleCollider2D _collider;
     
     [Header("Audio")]
     [SerializeField] 
@@ -16,7 +18,7 @@ public class Grab : MonoBehaviour
     private void Awake()
     {
         _playerAudio = GetComponent<AudioSource>();
-
+        _collider.enabled = false;
     }
     
     private void Update()
@@ -24,10 +26,12 @@ public class Grab : MonoBehaviour
         if (Input.GetKey(KeyCode.Space))
         {
             _isHolding = true;
+            _collider.enabled = true;
         }
         else
         {
             _isHolding = false;
+            _collider.enabled = false;
             Tentacle.GrabbedObject = null;
             Destroy(GetComponent<FixedJoint2D>());
         }
@@ -50,5 +54,27 @@ public class Grab : MonoBehaviour
                 FixedJoint2D fj = transform.gameObject.AddComponent(typeof(FixedJoint2D)) as FixedJoint2D;
             }
         }
+    }
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        
+        
+        if (other.gameObject.CompareTag("Object"))
+        {
+            Rigidbody2D rb = other.transform.GetComponent<Rigidbody2D>();
+            Tentacle.GrabbedObject = other.gameObject;
+            if (rb != null)
+            {
+                FixedJoint2D fj = transform.gameObject.AddComponent(typeof(FixedJoint2D)) as FixedJoint2D;
+                fj.connectedBody = rb;
+                _playerAudio.PlayOneShot(_grabbing);
+            }
+            else
+            {
+                FixedJoint2D fj = transform.gameObject.AddComponent(typeof(FixedJoint2D)) as FixedJoint2D;
+            }
+        }
+        
     }
 }

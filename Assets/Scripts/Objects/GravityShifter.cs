14b9b2eb98ec5a373827c67e3c82d416c0976a8b
André Borja <andre.borja.miranda@gmail.com>
2023-09-23 14:57:02 -0700

    MovingGrapple V1

--- Assets/Scripts/Objects/GravityShifter.cs
+++ Assets/Scripts/Objects/GravityShifter.cs
@@ -8,6 +8,7 @@ public class GravityShifter : MonoBehaviour, IOnOffObjects
     [SerializeField]
     private bool _isOn;
     private BoxCollider2D _collider;
+    [SerializeField] private LayerMask _shiftLayerMask;
 
     private void Awake()
     {
@@ -16,16 +17,23 @@ public class GravityShifter : MonoBehaviour, IOnOffObjects
 
     public void TurnOn()
     {
-        Collider2D[] collider2Ds = Physics2D.OverlapAreaAll(_collider.bounds.min, _collider.bounds.max);
-        foreach (BoxCollider2D collider2D in collider2Ds)
+        Debug.Log($"On");
+        Collider2D[] collider2Ds = Physics2D.OverlapAreaAll(_collider.bounds.min,_collider.bounds.max);
+        foreach (Collider2D collider2D in collider2Ds)
         {
+            Debug.Log($"Called");
             if (collider2D.GetComponent<Rigidbody2D>() != null)
             {
                 collider2D.GetComponent<Rigidbody2D>().gravityScale *= -1.0f;
             }
-
             Debug.Log($"{collider2D.name}");
         }
+    }
+    
+    private void OnDrawGizmos()
+    {
+        // Gizmos.DrawWireSphere(transform.position, 5.0f);
+        // Gizmos.DrawCube(_collider.bounds.center, new Vector3(5f,5f, 5f));
     }
 
     public void TurnOff()
@@ -33,15 +41,15 @@ public class GravityShifter : MonoBehaviour, IOnOffObjects
         _isOn = false;
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (!_isOn) return;
-        Debug.Log($"Detect");
-        if (other.GetComponent<Rigidbody2D>())
-        {
-            other.GetComponent<Rigidbody2D>().gravityScale *= -1f;
-        }
-    }
+    // private void OnTriggerEnter2D(Collider2D other)
+    // {
+    //     if (!_isOn) return;
+    //     Debug.Log($"Detect");
+    //     if (other.GetComponent<Rigidbody2D>())
+    //     {
+    //         other.GetComponent<Rigidbody2D>().gravityScale *= -1f;
+    //     }
+    // }
     
     private void OnTriggerExit2D(Collider2D other)
     {

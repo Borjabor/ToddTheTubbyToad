083fc893e0d988689f56ca09e4f628d957fa73b1
Sebastian Beltran <s.beltran.convers@gmail.com>
2023-10-08 19:59:51 -0700

    Water waves scripts working

--- Assets/Scripts/Water/WaterShapeController.cs
+++ Assets/Scripts/Water/WaterShapeController.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class WaterShapeController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private float _springStiffness = 0.1f;
+    [SerializeField]
+    private List<WaterSpring> _springs = new();
+    [SerializeField]
+    private float _dampening = .03f;
+
+    public float Spread = .006f;
+    
+    void FixedUpdate()
     {
-        
+        foreach (WaterSpring waterSpringComponent in _springs)
+        {
+            waterSpringComponent.WaveSpringUpdate(_springStiffness, _dampening);
+        }
+
+        UpdateSprings();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void UpdateSprings()
     {
-        
+        int count = _springs.Count;
+        float[] left_deltas = new float[count];
+        float[] right_deltas = new float[count];
+
+        for (int i = 0; i < count; i++)
+        {
+            if (i > 0)
+            {
+                left_deltas[i] = Spread * (_springs [i].Height - _springs[i-1].Height);
+                _springs[i-1].Velocity += left_deltas[i];
+            }
+
+            if (i < _springs.Count - 1) 
+            {
+                right_deltas[i] = Spread * (_springs[i].Height - _springs[i+1].Height);
+                _springs[i+1].Velocity += right_deltas[i];
+            }
+        }
+    }
+
+    private void Splash(int index, float speed) 
+    { 
+        if (index >= 0 && index < _springs.Count) 
+        {
+            _springs[index].Velocity += speed;
+        }
     }
 }
--- Assets/Scripts/Water/WaterSpring.cs
+++ Assets/Scripts/Water/WaterSpring.cs
@@ -1,35 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using Aarthificial.PixelGraphics.Universal;
 using UnityEngine;
 
 public class WaterSpring : MonoBehaviour
 {
-    private float _velocity = 0;
-    private float _force = 0;
-    private float _height = 0;
+    public float Velocity = 0;
+    public float Force = 0;
+    public float Height = 0;
     private float _targetHeight =0;
     
-    public void WaveSpringUpdate(float _springStiffness)
+    public void WaveSpringUpdate(float _springStiffness, float _dampening)
     {
-        _height = transform.position.y;
+        Height = transform.localPosition.y;
         //maximum extension
-        var x = _height - _targetHeight;
+        var x = Height - _targetHeight;
+        var loss = -_dampening * Velocity;
 
-        _force = -_springStiffness * x;
-        _velocity += _force;
-        var y = transform.position.y;
-        transform.localPosition = new Vector3(transform.localPosition.x, y+_velocity, transform.localPosition.z);
+        Force = -_springStiffness * x + loss;
+        Velocity += Force;
+        var y = transform.localPosition.y;
+        transform.localPosition = new Vector3(transform.localPosition.x, y+Velocity, transform.localPosition.z);
     }
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }

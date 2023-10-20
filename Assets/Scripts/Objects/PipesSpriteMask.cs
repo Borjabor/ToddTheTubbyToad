4ce16948dfa15db8535f631ac73719834d5e6363
Sebastian Beltran <s.beltran.convers@gmail.com>
2023-10-19 22:12:42 -0700

    Connecting lever

--- Assets/Scripts/Objects/PipesSpriteMask.cs
+++ Assets/Scripts/Objects/PipesSpriteMask.cs
@@ -7,26 +7,34 @@ using UnityEngine.Serialization;
 
 public class PipesSpriteMask : MonoBehaviour
 {
-    private GameObject _spriteMask;
-
+    [SerializeField] 
+    private SpriteRenderer _spriteRenderer;
+    [SerializeField] 
+    private SpriteMask _spriteMask;
     [SerializeField] 
     private bool _isOn = false;
     [SerializeField] 
     private bool startOn;
 
+
     private void Awake()
     {
-        _spriteMask = GetComponent<PipesSpriteMask>();
+        _spriteRenderer.enabled = false;
+        _spriteMask = GetComponent<SpriteMask>();
         if(startOn) TurnOn();
     }
 
     public void TurnOn()
     {
+        _spriteRenderer.enabled = true;
         _spriteMask.enabled = true;
+        _isOn = true;
     }
 
     public void TurnOff()
     {
+        _spriteRenderer.enabled = false;
         _spriteMask.enabled = false;
+        _isOn = false;
     }
 }

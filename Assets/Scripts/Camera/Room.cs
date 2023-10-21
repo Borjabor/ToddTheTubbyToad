7faf7dafd7926af14955e030ebd4c4bd893a7d85
Eduardo Cavalcante de Almeida <educavalcante02@hotmail.com>
2023-10-20 20:28:59 -0700

    made the Room script trigger stay

--- Assets/Scripts/Camera/Room.cs
+++ Assets/Scripts/Camera/Room.cs
@@ -13,7 +13,7 @@ public class Room : MonoBehaviour
         CameraManager.Instance.SetCamera(_virtualCamera);
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerStay2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {

b7e3885ac2024379f6ed5bd716f895cac1ac0baf
André Borja <andre.borja.miranda@gmail.com>
2023-10-31 11:23:07 -0700

    Moving Platform Fix

--- Assets/Scripts/Character/Tongue.cs
+++ Assets/Scripts/Character/Tongue.cs
@@ -87,6 +87,14 @@ public class Tongue : MonoBehaviour
                 _straightLine = true;
                 _player.Grapple();
             }
+            
+            if (collider2D.gameObject.layer == 10)
+            {
+                _player.SetMovingObject(collider2D.gameObject);
+                _isGrappling = true;
+                _straightLine = true;
+                _player.Grapple();
+            }
         }
     }
 

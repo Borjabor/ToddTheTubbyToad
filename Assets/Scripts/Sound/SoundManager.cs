ad5a15bf224030109ef80e9fc12be919094ac638
André Borja <andre.borja.miranda@gmail.com>
2023-11-15 00:48:09 -0800

    Sound Settings Saving

--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -8,16 +8,28 @@ using UnityEngine.Audio;
 public class SoundManager : Singleton<SoundManager>
 {
     [SerializeField]
-    private AudioSource _musicSource, _effectSource;
+    private AudioSource _musicSource01, _musicSource02;
+    private AudioSource _activeMusicSource;
+
+    public AudioMixer _musicMixer, _effectMixer;
 
     [SerializeField]
-    private AudioMixer _musicMixer, _effectMixer;
+    private float _timeToFade = 0.75f;
+    private float _timaElapsed;
+
+    private bool _isPlayingTrack01;
 
     
 
+    private void Update()
+    {
+        //if(Input.GetKeyDown(KeyCode.C)) PlayerPrefs.DeleteAll();
+    }
+    
     protected override void SingletonAwake()
     {
-        DontDestroyOnLoad(this);
+        DontDestroyOnLoad(gameObject);
+        _isPlayingTrack01 = true;
     }
 
     public void ChangeMasterVolume(float volume)
@@ -27,13 +39,48 @@ public class SoundManager : Singleton<SoundManager>
 
     public void ChangeMusicVolume(float volume)
     {
-        _musicSource.volume = volume;
         _musicMixer.SetFloat("MusicVolume", Mathf.Log10(volume) * 20);
     }
     
-    public void ChangeEffectVolume(float volume)
+    public void ChangeEffectsVolume(float volume)
     {
-        _effectSource.volume = volume;
         _effectMixer.SetFloat("EffectsVolume", Mathf.Log10(volume) * 20);
+    }
+    
+    public void ChangeMusic(AudioClip clip)
+    {
+        StopAllCoroutines();
+        StartCoroutine(CrossFade(clip));
+    }
+
+    private IEnumerator CrossFade(AudioClip clip)
+    {
+        if (_isPlayingTrack01)
+        {
+            _musicSource02.clip = clip;
+            _musicSource02.Play();
+            while (_timaElapsed < _timeToFade)
+            {
+                _musicSource02.volume = Mathf.Lerp(0, 1, _timaElapsed / _timeToFade);
+                _musicSource01.volume = Mathf.Lerp(1, 0, _timaElapsed / _timeToFade);
+                _timaElapsed += Time.deltaTime;
+                yield return null;
+            }
+            _musicSource01.Stop();
+        }
+        else
+        {
+            _musicSource01.clip = clip;
+            _musicSource01.Play();
+            while (_timaElapsed < _timeToFade)
+            {
+                _musicSource01.volume = Mathf.Lerp(0, 1, _timaElapsed / _timeToFade);
+                _musicSource02.volume = Mathf.Lerp(1, 0, _timaElapsed / _timeToFade);
+                _timaElapsed += Time.deltaTime;
+                yield return null;
+            }
+            _musicSource02.Stop();
+        }
+        
     }
 }
--- Assets/Scripts/Sound/VolumeSlider.cs
+++ Assets/Scripts/Sound/VolumeSlider.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
@@ -11,7 +12,7 @@ public class VolumeSlider : MonoBehaviour
     [SerializeField]
     private Slider _musicSlider;
     [SerializeField]
-    private Slider _effectSlider;
+    private Slider _effectsSlider;
     [SerializeField]
     private TMP_Text _masterSliderValue;
     [SerializeField]
@@ -21,17 +22,66 @@ public class VolumeSlider : MonoBehaviour
 
     private void Start()
     {
-        SoundManager.Instance.ChangeMasterVolume(_masterSlider.value);
-        SoundManager.Instance.ChangeMusicVolume(_musicSlider.value);
-        SoundManager.Instance.ChangeEffectVolume(_effectSlider.value);
+        if (PlayerPrefs.HasKey("masterVolume"))
+        {
+            var value = PlayerPrefs.GetFloat("masterVolume");
+            _masterSlider.value = value;
+            SoundManager.Instance.ChangeMasterVolume(value);
+        }
+        else
+        {
+            _masterSlider.value = 1;
+            SoundManager.Instance.ChangeMasterVolume(1);
+        }
+        
+        if (PlayerPrefs.HasKey("musicVolume"))
+        {
+            var value = PlayerPrefs.GetFloat("musicVolume");
+            _musicSlider.value = value;
+            SoundManager.Instance.ChangeMusicVolume(value);
+        }
+        else
+        {
+            _musicSlider.value = 1;
+            SoundManager.Instance.ChangeMusicVolume(1);
+        }
+        
+        if (PlayerPrefs.HasKey("effectsVolume"))
+        {
+            var value = PlayerPrefs.GetFloat("effectsVolume");
+            _effectsSlider.value = value;
+            SoundManager.Instance.ChangeEffectsVolume(value);
+        }
+        else
+        {
+            _masterSlider.value = 1;
+            SoundManager.Instance.ChangeEffectsVolume(1);
+        }
+        
         _masterSliderValue.text = Mathf.Round(_masterSlider.value * 100f).ToString() + " %";
         _musicSliderValue.text = Mathf.Round(_musicSlider.value * 100f).ToString()+ " %";
-        _effectSliderValue.text = Mathf.Round(_effectSlider.value * 100f).ToString()+ " %";
-        _masterSlider.onValueChanged.AddListener(val => SoundManager.Instance.ChangeMasterVolume(val));
-        _musicSlider.onValueChanged.AddListener(val => SoundManager.Instance.ChangeMusicVolume(val));
-        _effectSlider.onValueChanged.AddListener(val => SoundManager.Instance.ChangeEffectVolume(val));
-        _masterSlider.onValueChanged.AddListener(val => _masterSliderValue.text = Mathf.Round(val*100).ToString()+ " %");
-        _musicSlider.onValueChanged.AddListener(val =>_musicSliderValue.text = Mathf.Round(val*100).ToString()+ " %");
-        _effectSlider.onValueChanged.AddListener(val => _effectSliderValue.text = Mathf.Round(val*100).ToString()+ " %");
+        _effectSliderValue.text = Mathf.Round(_effectsSlider.value * 100f).ToString()+ " %";
+        
+        _masterSlider.onValueChanged.AddListener(val =>
+        {
+            SoundManager.Instance.ChangeMasterVolume(val);
+            _masterSliderValue.text = Mathf.Round(val * 100).ToString() + " %";
+            PlayerPrefs.SetFloat("masterVolume", val);
+            PlayerPrefs.Save();
+        });
+        _musicSlider.onValueChanged.AddListener(val =>
+        {
+            SoundManager.Instance.ChangeMusicVolume(val);
+            _musicSliderValue.text = Mathf.Round(val * 100).ToString() + " %";
+            PlayerPrefs.SetFloat("musicVolume", val);
+            PlayerPrefs.Save();
+        });
+        _effectsSlider.onValueChanged.AddListener(val =>
+        {
+            SoundManager.Instance.ChangeEffectsVolume(val);
+            _effectSliderValue.text = Mathf.Round(val * 100).ToString() + " %";
+            PlayerPrefs.SetFloat("effectsVolume", val);
+            PlayerPrefs.Save();
+        });
     }
 }
